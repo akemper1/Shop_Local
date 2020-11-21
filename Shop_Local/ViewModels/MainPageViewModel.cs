92f56d5605c6a4437468fef4c8d2e948903fd7ed
akemper1 <andrewkemper1@gmail.com>
2020-11-21 15:29:57 -0500

    Adding tabbed page to be the main page

--- Shop_Local/ViewModels/MainPageViewModel.cs
+++ Shop_Local/ViewModels/MainPageViewModel.cs
@@ -1,19 +1,7 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Shop_Local.ViewModels
+﻿namespace Shop_Local.ViewModels
 {
-    public class MainPageViewModel : ViewModelBase
+    public class MainPageViewModel
     {
-        public MainPageViewModel(INavigationService navigationService)
-            : base(navigationService)
-        {
-            Title = "Main Page";
-        }
+        
     }
 }
--- Shop_Local/Views/MainPage.xaml.cs
+++ Shop_Local/Views/MainPage.xaml.cs
@@ -1,6 +1,16 @@
-﻿namespace Shop_Local.Views
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Shop_Local.Views
 {
-    public partial class MainPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
