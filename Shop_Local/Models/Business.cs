c86c361aef4a9c97d0347a9b9811459bbe6d8ea7
akemper1 <andrewkemper1@gmail.com>
2020-12-09 18:01:52 -0500

    Updating business model

---
+++ Shop_Local/Models/Business.cs
@@ -0,0 +1,34 @@
+﻿using Plugin.CloudFirestore.Attributes;
+
+namespace Shop_Local.Models
+{
+    public class Business
+    {
+        [MapTo("name")]
+        public string Name { get; set; }
+
+        [MapTo("email")]
+        public string Email { get; set; }
+
+        [MapTo("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        [MapTo("website")]
+        public string Website { get; set; }
+
+        [MapTo("street_number")]
+        public int StreetNumber { get; set; }
+
+        [MapTo("street_name")]
+        public string StreetName { get; set; }
+
+        [MapTo("city")]
+        public string City { get; set; }
+
+        [MapTo("zip_code")]
+        public int ZipCode { get; set; }
+
+        [MapTo("category")]
+        public string Category { get; set; }
+    }
+}
---
+++ Shop_Local/Services/FirestoreDatabase.cs
@@ -0,0 +1,14 @@
+﻿using Plugin.CloudFirestore;
+using Shop_Local.Models;
+using System.Threading.Tasks;
+
+namespace Shop_Local.Services
+{
+    public class FirestoreDatabase
+    {
+        public async Task RecommendShop(Business business)
+        {
+            await CrossCloudFirestore.Current.Instance.Collection("recommended_businesses").AddAsync(business);
+        }
+    }
+}
